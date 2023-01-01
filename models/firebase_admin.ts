f24b6569ee3968726d32c3d177e4064cb11430c0
kabinny <kabinny@gmail.com>
2023-01-01 17:30:49 +0900

    준비하기-firebase admin 설정

---
+++ models/firebase_admin.ts
@@ -0,0 +1,62 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstance(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      // 초기화 진행
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      // 환경을 초기화한다
+      FirebaseAdmin.instance.bootstrap();
+    }
+    return FirebaseAdmin.instance;
+  }
+
+  // bootstrap
+  private bootstrap(): void {
+    const haveApp = admin.apps.length !== 0;
+    if (haveApp) {
+      this.init = true;
+      return;
+    }
+
+    const config: Config = {
+      // privateKey는 개행문자 처리를 했다.
+      // 원래 개행문자를 포함한 여러 줄의 코드인데 json에서의 표현 때문에 개행 문자 처리 필요.
+      credential: {
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        privateKey: (process.env.privateKey || '').replace(/\\n/g, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin');
+  }
+
+  /** firestore 반환 */
+  public get Firebase(): FirebaseFirestore.Firestore {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.firestore();
+  }
+
+  /** auth 반환 */
+  public get Auth(): admin.auth.Auth {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.auth();
+  }
+}
