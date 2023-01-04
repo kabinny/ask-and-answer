650a11f5aebcd3648f62163bd6475959156adfd6
kabinny <kabinny@gmail.com>
2023-01-04 15:11:29 +0900

    GNB 뷰 작업

--- components/service_layout.tsx
+++ components/service_layout.tsx
@@ -1,17 +1,21 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Box, BoxProps } from '@chakra-ui/react';
 import Head from 'next/head';
+import GNB from './GNB';
 
 interface Props {
   title: string;
   children: React.ReactNode;
 }
 
-export const ServiceLayout = function ({ title = 'aaa', children }: Props) {
+export const ServiceLayout: React.FC<Props & BoxProps> = function ({ title = 'aaa', children, ...boxProps }) {
   return (
-    <div>
+    <Box {...boxProps}>
       <Head>
         <title>{title}</title>
       </Head>
+      <GNB />
       {children}
-    </div>
+    </Box>
   );
 };
