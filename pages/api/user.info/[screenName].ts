ab28a306398743088cfd925f5a3e8721c9fe5104
kabinny <kabinny@gmail.com>
2023-01-05 20:18:21 +0900

    사용자 정보 조회 API 추가

---
+++ pages/api/user.info/[screenName].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import MemberCtrl from '@/controllers/member.crtl';
+import handleError from '@/controllers/error/handle_error';
+import checkSupportMethod from '@/controllers/error/check_support_methos';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const supportMethod = ['GET'];
+  try {
+    checkSupportMethod(supportMethod, method);
+    await MemberCtrl.findByScreenName(req, res);
+  } catch (error) {
+    console.error(error);
+    // 에러 처리
+    handleError(error, res);
+  }
+}
