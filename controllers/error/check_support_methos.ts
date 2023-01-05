8ad92c225cc57f28e54ca4c6c7f4908bf2beeef8
kabinny <kabinny@gmail.com>
2023-01-05 19:57:02 +0900

    서버사이드 공통 에러 핸들링

---
+++ controllers/error/check_support_methos.ts
@@ -0,0 +1,8 @@
+import BadReqErr from './bad_request_error';
+
+export default function checkSupportMethod(supportMethod: string[], method?: string) {
+  if (supportMethod.indexOf(method!) === -1) {
+    // 에러 반환
+    throw new BadReqErr('지원하지 않는 method');
+  }
+}
