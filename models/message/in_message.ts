7bcdc92ecc5d16c880a9ebe2fa5ccf7b2f198b29
kabinny <kabinny@gmail.com>
2023-01-09 18:39:43 +0900

    메세지 목록 조회 API 구현

--- models/message/in_message.ts
+++ models/message/in_message.ts
@@ -1,13 +1,23 @@
-export interface InMessage {
+import { firestore } from 'firebase-admin';
+
+interface MessageBase {
   id: string;
   /** 사용자가 남긴 질문 */
   message: string;
   /** 댓글 */
   reply?: string;
-  createdAt: string;
-  replyAt?: string;
   author?: {
     displayName: string;
     photoURL?: string;
   };
+}
+
+export interface InMessage extends MessageBase {
+  createdAt: string;
+  replyAt?: string;
+}
+
+export interface InMessageServer extends MessageBase {
+  createdAt: firestore.Timestamp;
+  replyAt?: firestore.Timestamp;
 }
