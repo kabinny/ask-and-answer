d556fc53859f513d8d5e4ba105be226def9467f7
kabinny <kabinny@gmail.com>
2023-01-31 20:15:12 +0900

    remove custom font-family

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -5,11 +5,7 @@ export const config = {
   runtime: 'edge',
 };
 
-const font = fetch(new URL('../../assets/Pretendard-Regular.ttf', import.meta.url)).then((res) => res.arrayBuffer());
-
-export default async function handler(req: NextRequest) {
-  const fontData = await font;
-
+export default function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
 
@@ -46,7 +42,6 @@ export default async function handler(req: NextRequest) {
               color: 'black',
               zIndex: 10,
               width: '100%',
-              fontFamily: 'Pretendard',
             }}
           >
             {text}
@@ -56,13 +51,6 @@ export default async function handler(req: NextRequest) {
       {
         width: 1200,
         height: 681,
-        fonts: [
-          {
-            name: 'Pretendard',
-            data: fontData,
-            style: 'normal',
-          },
-        ],
       },
     );
   } catch (e: any) {
